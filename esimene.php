17ad8b78665d318e1174f63caf28402de4d1b463
Aleksandr Bogdan <abogdan@enos.itcollege.ee>
2016-02-21 19:48:48 +0200

    esimene nadal

--- esimene.php
+++ esimene.php
@@ -1,18 +1,21 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
   <head>
+    <meta name="My Page" content="praktika I244"/>
     <title>My page</title>
-    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <link rel="stylesheet" type="text/css" href="mystyle.css"/>
   </head>
   <body>
-    <H1>My Page</H1>
+    
+    <h1>My Page</h1>
     <p>Hello world!</p>
 
     <p>
-      <img id="blocks" onclick="changeImage()" src="abc-blocks.jpg">
+      <img id="blocks" onclick="changeImage()" src="abc-blocks.jpg" alt="blocks" />
     </p>
     <p>Click image to change it!</p>
 
-    <script>
+    <script type="text/javascript">
     function changeImage() {
         var image = document.getElementById('blocks');
         if (image.src.match("abc-blocks")) {
@@ -24,7 +27,7 @@
     </script>
     <p>
     <?php
-    echo "I am PHP counter!<br>";
+    echo "I am PHP counter!<br/>";
 
     $file = 'counter.txt';
     
@@ -39,6 +42,12 @@
     ?>
     </p>
     
+    
+    <p>
+ <a href="http://validator.w3.org/check?uri=referer">
+  <img id ="logo" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+ </a>
+</p>
 
   </body>
 </html>
